d4e7dc4af4f17fe0c47b7d87810d38ab3d89cd71
Tommaso Pacciani <trooster.t@gmail.com>
2023-06-27 18:22:48 +0200

    added runfct script

---
+++ gen_runfct.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DEPENDENCIES="$(pwd)/dependencies"
+#TODO replace with find
+cbsc="$DEPENDENCIES/funcons-intgen/dist-newstyle/build/x86_64-linux/ghc-9.0.2/funcons-intgen-0.2.0.3/x/cbsc/build/cbsc/cbsc"
+#
+#$(${cbsc} )
+#
+
+
+# Set the directory path
+workspace_dir="spoofax-workspace"
+
+# Search for *-definition folders and loop over them
+while IFS= read -r -d '' definition_folder; do
+  # Get the name by extracting the part before '-definition'
+  name=$(basename "$definition_folder" | sed 's/-definition$//')
+
+  # Find the matching *-Funcons folder and save its location
+  funcons_folder=$(find "$workspace_dir" -type d -name "${name}-Funcons" -print -quit)
+  funcons_file=$(find "$funcons_folder" -type f -name "${name}-Funcons.cbs" -print -quit)
+  # Find the matching *-Runfct folder and save its location
+  runfct_folder=$(find "$workspace_dir" -type d -name "${name}-Runfct" -print -quit)
+  
+  # Run cbs  
+  ${cbsc} ${funcons_file} ${runfct_folder} ${name}
+  # Replace the none functioning import in Main.hs
+  sed -i "s/import Funcons.$name.Library/import Funcons.$name.${name}Funcons.${name}Funcons/" "$runfct_folder/Main.hs"
+  # Execute cabal build in runfct_folder
+  cd "$runfct_folder" || exit
+  cabal build
+  cabal install --overwrite-policy=always
+  
+  # Return to the previous directory
+  cd - || exit
+done < <(find "$workspace_dir" -type d -name '*-definition' -print0)
